f74a09a23af7a28e1368396384581a419cad0ff6
Vadim Igumnov <vad.igumnoff@gmail.com>
2022-08-20 18:17:23 +0300

    feat: implement 'Result Area' component, implement base font change logic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <modal-window v-if="isModalOpen" @close-modal="closeModal">
+  <modal-window v-show="isModalOpen" @close-modal="closeModal">
     <template v-slot:window-title>Добро пожаловать</template>
 
     <template v-slot:window-description>
@@ -9,14 +9,25 @@
 
     <template v-slot:button-text>Продолжить</template>
   </modal-window>
+  <main class="mx-12 my-8 grid grid-cols-[300px_1fr] gap-8">
+    <query-area v-model:query="query" />
+    <result-area class="row-span-2" :query="query" />
+    <setting-area />
+  </main>
 </template>
 
 <script>
+import QueryArea from '@/components/QueryArea';
+import ResultArea from '@/components/ResultArea';
+import SettingArea from '@/components/SettingArea';
+
 export default {
+  components: { QueryArea, ResultArea, SettingArea },
   name: 'App',
   data() {
     return {
-      isModalOpen: true,
+      isModalOpen: false,
+      query: '',
     };
   },
   methods: {
---
+++ src/components/ResultArea.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex justify-center items-center self-stretch text-3xl">
+    <span
+      class="whitespace-pre-wrap"
+      :style="{ 'font-family': letter.font }"
+      v-for="(letter, idx) of calculatedQuery"
+      :key="idx"
+      >{{ letter.letter }}</span
+    >
+  </div>
+</template>
+
+<script>
+const FONTS = [
+  'Alegreya',
+  'Alumni Sans Collegiate One',
+  'Amatic SC',
+  'Bellota Text',
+  'Brygada 1918',
+  'Caveat',
+  'Comfortaa',
+  'Comforter Brush',
+  'Cormorant Garamond',
+  'Cormorant Infant',
+  'Exo 2',
+  'IBM Plex Serif',
+  'Lobster',
+  'Merriweather',
+  'Neucha',
+  'Noto Serif Display',
+  'Oi',
+  'Orelega One',
+  'Pacifico',
+  'Pattaya',
+  'Playfair Display',
+  'Playfair Display SC',
+  'Prata',
+  'Press Start 2P',
+  'Roboto',
+  'Rubik Mono One',
+  'Ruslan Display',
+  'Russo One',
+  'Yanone Kaffeesatz',
+  'Yeseva One',
+];
+
+export default {
+  name: 'result-area',
+  props: {
+    query: String,
+  },
+  data() {
+    return {
+      newQuery: [],
+      // font: '',
+    };
+  },
+  watch: {
+    query() {
+      // ЗДЕСЬ ПРОИСХОДИТ ЛОГИКА ОБРАБОТКИ ТЕКСТА
+      // this.newQuery = this.query.split('').map(l => {
+      //   let random = Math.ceil(Math.random() * 10);
+      //   let font = `'${FONTS[random]}', serif`;
+      //   return { letter: l, font: font };
+      // });
+    },
+  },
+  computed: {
+    calculatedQuery() {
+      return this.query.split('').map(l => {
+        let random = Math.ceil(Math.random() * 10);
+        let font = `'${FONTS[random]}', serif`;
+        return { letter: l, font: font };
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
