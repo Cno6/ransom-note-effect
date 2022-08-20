268eeaa7e19257f9f12eda6a3a5e9b5cad6af2cc
Vadim Igumnov <vad.igumnoff@gmail.com>
2022-08-20 18:18:23 +0300

    add base markup for Query Area and Setting Area components

---
+++ src/components/QueryArea.vue
@@ -0,0 +1,33 @@
+<template>
+  <textarea
+    :value="query"
+    @input="makeQuary"
+    name="query-area"
+    cols="30"
+    rows="10"
+    placeholder="Введите текст здесь..."
+    class="outline-none border-none p-5 rounded-md resize-none shadow-md"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'query-area',
+  props: {
+    query: String,
+  },
+  emits: {
+    'update:query': null,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    makeQuary(event) {
+      this.$emit('update:query', event.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/SettingArea.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-5 rounded-md shadow-md bg-white">
+    <h2 class="pb-4 border-b border-b-gray-200">Настройки</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'setting-area',
+};
+</script>
+
+<style lang="scss" scoped></style>
